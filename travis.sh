789fe55bc1a6dbdd02005a166c455b5de3a798a9
Sérgio Kopplin <sergiokopplin@gmail.com>
2017-02-12 00:02:11 -0200

    adding exception to facebook url test

--- travis.sh
+++ travis.sh
@@ -3,4 +3,4 @@ set -e # halt script on error
 
 echo 'Testing travis...'
 bundle exec jekyll build
-bundle exec htmlproofer ./_site --only-4xx
+bundle exec htmlproofer --url-ignore "/facebook.com/" ./_site --only-4xx
